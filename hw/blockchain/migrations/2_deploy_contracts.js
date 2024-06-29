4e9eb4679f559e2bc30572a0f24660b3b8be8ac2
Bogdan <62383356+BogdanPronin@users.noreply.github.com>
2024-06-29 16:10:19 +0300

    add

--- hw/blockchain/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const TestWallet = artifacts.require("TestWallet");
-
-module.exports = async function (deployer) {
-  // Deploy the TestWallet contract
-  await deployer.deploy(TestWallet);
-  const walletInstance = await TestWallet.deployed();
-  console.log("Wallet deployed at address:", walletInstance.address);
-};
